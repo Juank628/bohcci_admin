245cfea435ee2eea83e41b99b9b373c292c9c277
Juank628 <jsanchez_zunino@yahoo.com>
2023-03-22 22:07:43 -0500

    Created Redux Store and Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,24 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from '@apollo/client';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
-let client = new ApolloClient({
+export const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql',
   cache: new InMemoryCache(),
 });
 
-client
-  .query({
-    query: gql`
-      {
-        getAllProducts {
-          id
-          name
-          description
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-  </React.StrictMode>
+  </Provider>
 );
