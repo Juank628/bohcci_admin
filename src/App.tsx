5923a3ce45e316e9ed5a2b5c1a72c5d38fef1261
Juank628 <jsanchez_zunino@yahoo.com>
2023-04-12 23:49:53 -0500

    Setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+  BrowserRouter, Routes, Route, Navigate,
+} from 'react-router-dom';
 import Products from './pages/Products';
 import './assets/normalize.css';
 import './App.css';
@@ -6,7 +9,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <Products />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/products" />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
